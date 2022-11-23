38be4b7b8d0f38411f0698ac906f8cb3938d1825
mingyu <popmin9179@naver.com>
2022-11-23 15:47:03 +0900

    test commit(20221323 13:49)

--- src/main/java/com/springbootstudy/book/springboot/web/HelloController.java
+++ src/main/java/com/springbootstudy/book/springboot/web/HelloController.java
@@ -1,12 +1,21 @@
 package com.springbootstudy.book.springboot.web;
 
 import com.springbootstudy.book.springboot.web.dto.HelloResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
+
+    private final Environment env;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
@@ -15,5 +24,17 @@ public class HelloController {
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
