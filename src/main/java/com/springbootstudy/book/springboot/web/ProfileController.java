2bcb3f1e0e7b9ce7138cf9eec3390d8b2bc2acda
mingyu <popmin9179@naver.com>
2022-11-23 15:58:42 +0900

    test commit(20221323 13:49)

--- src/main/java/com/springbootstudy/book/springboot/web/ProfileController.java
+++ src/main/java/com/springbootstudy/book/springboot/web/ProfileController.java
@@ -2,7 +2,7 @@ package com.springbootstudy.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -14,7 +14,7 @@ import java.util.List;
 public class ProfileController {
     private final Environment env;
 
-    //@GetMapping("/profile")
+    @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
